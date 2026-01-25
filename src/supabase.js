2014239f774e51a26689148dd2bbb3aa645d5cc9
Nishant <nishant.s.haldar@gmail.com>
2026-01-25 15:26:41 +0530

    secure supabase keys using env

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://erxipmyusdybncndtquc.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyeGlwbXl1c2R5Ym5jbmR0cXVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzMTMxODMsImV4cCI6MjA4NDg4OTE4M30.laHr-v4_stSlqIvdfweDXIZKSdxeTIXghrKOsSx-ANY"
-
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
